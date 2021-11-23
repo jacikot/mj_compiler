04cb768972f0e7daab6cfd8447f119efec0988b8
jacikot <jana.toljaga725@yahoo.com>
2021-11-23 03:16:31 +0300

    Finished syntax analysis of last year's project

---
+++ src/code/sym.java
@@ -0,0 +1,66 @@
+package code;
+
+public class sym {
+    //keywords
+    public static final int PROG=1;
+    public static final int BREAK=2;
+    public static final int CLASS=3;
+    public static final int ENUM=4;
+    public static final int ELSE=5;
+    public static final int CONST=6;
+    public static final int IF=7;
+    public static final int SWITCH=8;
+    public static final int DO=9;
+    public static final int WHILE=10;
+    public static final int NEW=11;
+    public static final int PRINT=12;
+    public static final int READ=13;
+    public static final int EXTND=14;
+    public static final int CONT=15;
+    public static final int CASE=16;
+    public static final int RETURN=17;
+
+    //id
+    public static final int IDENT=18;
+
+    //const
+    public static final int NUMBER=19;
+    public static final int CHAR=20;
+    public static final int BOOL=21;
+
+    // Operators
+    public static final int PLUS = 22;
+    public static final int MINUS = 23;
+    public static final int MUL = 24;
+    public static final int DIV = 25;
+    public static final int MOD = 26;
+    public static final int EQU = 27;
+    public static final int NEQ = 28;
+    public static final int GRT = 29;
+    public static final int GRE = 30;
+    public static final int LES = 31;
+    public static final int LEE = 32;
+    public static final int AND = 33;
+    public static final int OR = 34;
+    public static final int EQUAL = 35;
+    public static final int INC = 36;
+    public static final int DEC = 37;
+    public static final int COMMA = 38;
+    public static final int SEMI = 39;
+    public static final int DOT = 40;
+    public static final int LPAREN = 41;
+    public static final int RPAREN = 42;
+    public static final int LBRACE = 43;
+    public static final int RBRACE = 44;
+    public static final int LBRACK = 45;
+    public static final int RBRACK = 46;
+    public static final int QUEST = 47;
+    public static final int COLUMN = 48;
+
+    public static final int EOF = 49;
+    public static final int VOID = 50;
+
+
+
+
+}
